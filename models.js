2deda7bff269252e94e7e7499b9ae140e72badee
ramirezg <gabriel.ramirez@gsa.gov>
2014-12-03 17:15:33 -0500

    removed cron loop, updated with user triggered updates depending on the interval.
    separated the general and specific apis, general is now on a 10 min time interval, specific is now also on a time interval, that only updates when the user triggers it.
    
    cleaned up routs
    
    cleaning up code
    
    took out general model for now
    
    reshape
    
    only specific model
    
    added custom endpoint

--- models.js
+++ models.js
@@ -2,19 +2,30 @@ var mongoose = require('mongoose');
 var Mixed = mongoose.Schema.Types.Mixed;
 
 module.exports = {
-  Specific: mongoose.model(
-    'Specific', new mongoose.Schema({
-      slug: { type: String, required: true, unique: true },
-      apicall: { type: String, required: true, unique: true },
-      data: { type: Mixed , required: false }
-    })
-  ),
-
-  General: mongoose.model(
-    'Public', new mongoose.Schema({
-      slug: { type: String, required: true, unique: true },
-      apicall: { type: String, required: true, unique: true },
-      data: { type: Mixed , required: false }
-    })
-  )
+    Analytics: mongoose.model(
+        'AnalyticsSchema', new mongoose.Schema({
+            slug: {
+                type: String,
+                required: true,
+                unique: true
+            },
+            apicall: {
+                type: String,
+                required: true,
+                unique: true
+            },
+            update_interval: {
+                type: Number,
+                required: false
+            },
+            last_update: {
+                type: Number,
+                required: false
+            },
+            data: {
+                type: Mixed,
+                required: false
+            }
+        })
+    )
 };
