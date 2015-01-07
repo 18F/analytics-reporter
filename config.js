70cffe8dee59a64f660654c26b3361fa13743bf6
Eric Mill <eric.mill@gsa.gov>
2015-01-07 01:41:35 -0500

    working s3 publishing system

--- config.js
+++ config.js
@@ -7,6 +7,14 @@ module.exports = {
 
   debug: (process.env.ANALYTICS_DEBUG ? true : false),
 
+  // No trailing slashes
+  aws: {
+    bucket: process.env.AWS_BUCKET,
+    path: process.env.AWS_BUCKET_PATH,
+    // in seconds. Defaults to an hour (3600)
+    cache: process.env.AWS_CACHE_TIME
+  },
+
   account: {
     ids: process.env.ANALYTICS_REPORT_IDS
   },
