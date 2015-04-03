178a2b8c734e045a8501065ab734422ebefe76bc
Lauren Ancona <lala.ancona@gmail.com>
2015-04-03 13:13:52 -0400

    add ANALYTICS_KEY as config var

--- analytics.js
+++ analytics.js
@@ -12,10 +12,11 @@ var config = require('./config');
 // Pre-load the keyfile from the OS
 // prevents errors when starting JWT
 var key;
-if (fs.existsSync(config.key))
-    key = fs.readFileSync(config.key);
-else
-    key = null;
+    if (config.key)
+    key = config.key;
+    else if (config.key_file && fs.existsSync(config.key_file))
+    key = fs.readFileSync(config.key_file);
+    else key = null;
 
 var jwt = new googleapis.auth.JWT(
     config.email,
