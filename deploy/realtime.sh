4ff451662f13b1df7bb11a958fd53f37a629a449
Eric Mill <eric.mill@gsa.gov>
2015-10-29 04:01:22 -0400

    CSV version please

--- deploy/realtime.sh
+++ deploy/realtime.sh
@@ -6,4 +6,4 @@ source $HOME/.bashrc
 $HOME/node_modules/analytics-reporter/bin/analytics --publish --frequency=realtime --slim --verbose
 
 # we want just one realtime report in CSV, hardcoded for now to save on API requests
-$HOME/node_modules/analytics-reporter/bin/analytics --publish --only=all-pages-realtime --slim --verbose
+$HOME/node_modules/analytics-reporter/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
