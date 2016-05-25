22fd4129e0c779d400bf55f37cb772362efbf46f
Colin Craig <colin.craig@gsa.gov>
2016-05-25 15:48:53 -0400

    Added agencies to crontab

--- deploy/daily.sh
+++ deploy/daily.sh
@@ -56,3 +56,78 @@ $HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verb
 source $HOME/envs/nara.env
 $HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
 $HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Department of Agriculture
+source $HOME/envs/agriculture.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Department of Defense
+source $HOME/envs/defense.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Department of Health and Human Services
+source $HOME/envs/hhs.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Department of Housing and Urban Development
+source $HOME/envs/hud.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Department of Homeland Security
+source $HOME/envs/dhs.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Department of Labor
+source $HOME/envs/labor.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Department of State
+source $HOME/envs/state.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Department of Transportation
+source $HOME/envs/transportation.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Department of the Treasury
+source $HOME/envs/treasury.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Agency for International Development
+source $HOME/envs/usaid.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# General Services Administration
+source $HOME/envs/gsa.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# National Science Foundation
+source $HOME/envs/nsf.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Nuclear Regulatory Commission
+source $HOME/envs/nrc.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Office of Personnel Management
+source $HOME/envs/opm.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
+
+# Social Security Administration
+source $HOME/envs/ssa.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose
+$HOME/analytics-reporter/bin/analytics --publish --frequency=daily --slim --verbose --csv
--- deploy/hourly.sh
+++ deploy/hourly.sh
@@ -45,3 +45,64 @@ $HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --ver
 # NARA Reports
 source $HOME/envs/nara.env
 $HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Department of Agriculture
+source $HOME/envs/agriculture.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Department of Defense
+source $HOME/envs/defense.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Department of Health and Human Services
+source $HOME/envs/hhs.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Department of Housing and Urban Development
+source $HOME/envs/hud.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Department of Homeland Security
+source $HOME/envs/dhs.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Department of Labor
+source $HOME/envs/labor.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Department of State
+source $HOME/envs/state.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Department of Transportation
+source $HOME/envs/transportation.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Department of the Treasury
+source $HOME/envs/treasury.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Agency for International Development
+source $HOME/envs/usaid.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# General Services Administration
+source $HOME/envs/gsa.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# National Science Foundation
+source $HOME/envs/nsf.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Nuclear Regulatory Commission
+source $HOME/envs/nrc.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Office of Personnel Management
+source $HOME/envs/opm.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
+# Social Security Administration
+source $HOME/envs/ssa.env
+$HOME/analytics-reporter/bin/analytics --publish --frequency=hourly --slim --verbose
+
