18F/analytics-reporter

commit bfe43e1f706cbce31cbfbea56f99da5cbfa9daa6

Add result formatter
This commit adds a `ResultFormatter` service which can be used to format the results into either JSON or CSV. This moves the concern for formatting results out of `index.js`.

index.js
@@ -7,7 +7,7 @@ var Analytics = require("./src/analytics"),
     zlib = require('zlib');
 
 const PostgresPublisher = require("./src/publish/postgres")
-
+const ResultFormatter = require("./src/process-results/result-formatter")
 
 // AWS credentials are looked for in env vars or in ~/.aws/config.
 // AWS bucket and path need to be set in env vars mentioned in config.js.
@@ -76,21 +76,16 @@ var run = function(options) {
           writeToDatabasePromise = Promise.resolve();
         }
 
+        let format = "json"
+        if (options.csv) {
+          format = "csv"
+        }
+
         writeToDatabasePromise.then(() => {
-          // CSV, see https://github.com/C2FO/fast-csv#formatting-functions
-          if (options.csv) {
-            csv.writeToString(data['data'], {headers: true}, function(err, data) {
-              if (err) return console.log("ERROR AFTER CSV: " + JSON.stringify(err));
-              writeReport(name, data, ".csv", done);
-            });
-          }
-          // JSON
-          else {
-            // some reports can be slimmed down for direct rendering
-            if (options.slim && report.slim) delete data.data;
-            writeReport(name, JSON.stringify(data, null, 2), ".json", done);
-          }
-        }).catch(done);
+          return ResultFormatter.formatResult(data, format, options)
+        }).then(formattedResult => {
+          return writeReport(name, formattedResult, `.${format}`, done)
+        }).catch(done)
     });
   };
 

src/process-results/result-formatter.js
@@ -1,0 +1,37 @@
+const csv = require("fast-csv")
+
+const formatResult = (result, format, { slim = false } = {}) => {
+  result = Object.assign({}, result)
+
+  switch(format) {
+    case "json":
+      return _formatJSON(result, { slim })
+      break
+    case "csv":
+      return _formatCSV(result)
+      break
+    default:
+      return Promise.reject("Unsupported format: " + format)
+  }
+}
+
+const _formatJSON = (result, { slim }) => {
+  if (slim) {
+   delete result.data
+  }
+  return Promise.resolve(JSON.stringify(result, null, 2))
+}
+
+const _formatCSV = (result) => {
+  return new Promise((resolve, reject) => {
+    csv.writeToString(result.data, {headers: true}, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+module.exports = { formatResult }

test/process-results/result-formatter.test.js
@@ -1,0 +1,56 @@
+const csv = require("fast-csv")
+const expect = require("chai").expect
+const proxyquire = require("proxyquire")
+const reportFixture = require("../support/fixtures/report")
+const dataFixture = require("../support/fixtures/data")
+
+const GoogleAnalyticsDataProcessor = proxyquire("../../src/process-results/ga-data-processor", {
+  "../config": { account: { hostname: "" } },
+})
+const ResultFormatter = require("../../src/process-results/result-formatter")
+
+describe("ResultFormatter", () => {
+  describe("formatResult(result, format, options)", () => {
+    let report
+    let data
+
+    beforeEach(() => {
+      report = Object.assign({}, reportFixture)
+      data = Object.assign({}, dataFixture)
+    })
+
+    it("should format results into JSON if the format is 'json'", done => {
+      const result = GoogleAnalyticsDataProcessor.processData(report, data)
+
+      ResultFormatter.formatResult(result, "json").then(formattedResult => {
+        const object = JSON.parse(formattedResult)
+        expect(object).to.deep.equal(object)
+        done()
+      }).catch(done)
+    })
+
+    it("should remove the data attribute for JSON if options.slim is true", done => {
+      const result = GoogleAnalyticsDataProcessor.processData(report, data)
+
+      ResultFormatter.formatResult(result, "json", { slim: true }).then(formattedResult => {
+        const object = JSON.parse(formattedResult)
+        expect(object.data).to.be.undefined
+        done()
+      }).catch(done)
+    })
+
+    it("should format results into CSV if the format is 'csv'", done => {
+      const result = GoogleAnalyticsDataProcessor.processData(report, data)
+
+      ResultFormatter.formatResult(result, "csv", { slim: true }).then(formattedResult => {
+        csv.fromString(formattedResult, { strictColumnHandling: true }, { headers: true })
+          .on("invalid-data", (data) => {
+            done(new Error("Invalid CSV data: " + data))
+          })
+          .on("finish", () => {
+            done()
+          })
+      }).catch(done)
+    })
+  })
+})

commit 6a44ee74acdd8088204edb3bd5f49c8a378e85f7

Respect slim property on report objects
Prior to this commit, the reporter was treating all reports as slim due
to a bug introduced by bfe43e1f706cbce31cbfbea56f99da5cbfa9daa6. This
commit fixes that bug by checking that the report is slim and the
`--slim` option is used before treating a report like it's slim

index.js
@@ -82,7 +82,9 @@ var run = function(options) {
         }
 
         writeToDatabasePromise.then(() => {
-          return ResultFormatter.formatResult(data, format, options)
+          return ResultFormatter.formatResult(data, format, {
+            slim: options.slim && report.slim
+          })
         }).then(formattedResult => {
           return writeReport(name, formattedResult, `.${format}`, done)
         }).catch(done)