d601aae4a09c157f36c3cc1faa75a8f47b3a7940
Ryan Wold <ryan.wold@gsa.gov>
2023-04-05 13:27:23 -0700

    deploy using a deployer service account

--- bin/deploy-production-ci.sh
+++ bin/deploy-production-ci.sh
@@ -4,8 +4,7 @@ set -e
 
 # Log into cloud.gov
 cf api api.fr.cloud.gov
-cf login -u $CF_USERNAME -p $CF_PASSWORD -o gsa-opp-analytics -s analytics-dev
-# cf login -u $CF_PRODUCTION_SPACE_DEPLOYER_USERNAME -p $CF_PRODUCTION_SPACE_DEPLOYER_PASSWORD -o gsa-opp-analytics -s analytics-dev
+cf login -u $CF_PRODUCTION_SPACE_DEPLOYER_USERNAME -p $CF_PRODUCTION_SPACE_DEPLOYER_PASSWORD -o gsa-opp-analytics -s analytics-dev
 
 # Push the app
 cf v3-zdt-push analytics-reporter
