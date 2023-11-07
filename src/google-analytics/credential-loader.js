f36cd659973f64f30eafaae36b15cf72ef579dd3
Mesele <42145681+Mesele29@users.noreply.github.com>
2023-11-07 16:47:38 -0500

    print creds

--- src/google-analytics/credential-loader.js
+++ src/google-analytics/credential-loader.js
@@ -1,9 +1,16 @@
 const config = require("../config")
+const winston = require('winston');
+const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.json(),
+	transports: [new winston.transports.Console()],
+  });
 
 global.analyticsCredentialsIndex = 0
 
 const loadCredentials = () => {
   const credentialData = JSON.parse(config.analytics_credentials)
+  logger.info('in loadCredentials'+credentialData);
   const credentialsArray = _wrapArray(credentialData)
   const index = global.analyticsCredentialsIndex++ % credentialsArray.length
   return credentialsArray[index]
