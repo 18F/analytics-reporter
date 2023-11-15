4dacd87491b4094a431266e2d0db8c52b73969ba
Xavier Metichecchia <XavierMetichecchia>
2023-11-15 15:56:08 -0500

    Trying to get analytics-credentials in there correctly 5

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export PATH="$PATH:/home/vcap/deps/0/bin"
-echo $ANALYTICS_CREDENTIALS
+touch /home/vcap/app/creds.json
+export ANALYTICS_CREDENTIALS=$(printf "%s\n" $CREDS | base64 -w 0 --decode)
 node deploy/cron.js
-
