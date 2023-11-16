30a0dedcd7ed0dacb9ecf49ab0bf656d19b746e5
Xavier Metichecchia <XavierMetichecchia>
2023-11-16 11:03:55 -0500

    Change config.js to parse VCAP_SERVICE directly

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 // Set environment variables to configure the application.
 
+const VCAP_SERVICES_JSON = JSON.parse(process.env.VCAP_SERVICES)
+
 module.exports = {
 
   email: process.env.ANALYTICS_REPORT_EMAIL,
@@ -17,18 +19,32 @@ module.exports = {
     Separately, you need to set AWS_REGION, AWS_ACCESS_KEY_ID, and
     AWS_SECRET_ACCESS_KEY. The AWS SDK for Node reads these in automatically.
   */
+  // aws: {
+    // No trailing slashes
+    // bucket: process.env.AWS_BUCKET,
+    // path: process.env.AWS_BUCKET_PATH,
+    // HTTP cache time in seconds. Defaults to 0.
+    // cache: process.env.AWS_CACHE_TIME,
+    // endpoint: process.env.AWS_S3_ENDPOINT,
+    // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    // s3ForcePathStyle: process.env.AWS_S3_FORCE_STYLE_PATH,
+    // signatureVersion: process.env.AWS_SIGNATURE_VERSION
+  // },
+  //
   aws: {
     // No trailing slashes
-    bucket: process.env.AWS_BUCKET,
+    bucket:  VCAP_SERVICES_JSON.s3[0].credentials[0].bucket,
     path: process.env.AWS_BUCKET_PATH,
     // HTTP cache time in seconds. Defaults to 0.
     cache: process.env.AWS_CACHE_TIME,
-    endpoint: process.env.AWS_S3_ENDPOINT,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    endpoint: VCAP_SERVICES_JSON.s3[0].credentials[0].endpoint,
+    accessKeyId: VCAP_SERVICES_JSON.s3[0].credentials[0].access_key_id,
+    secretAccessKey: VCAP_SERVICES_JSON.s3[0].credentials[0].secret_access_key, 
     s3ForcePathStyle: process.env.AWS_S3_FORCE_STYLE_PATH,
     signatureVersion: process.env.AWS_SIGNATURE_VERSION
   },
+
 
   account: {
     ids: process.env.ANALYTICS_REPORT_IDS,
@@ -38,12 +54,19 @@ module.exports = {
     hostname: process.env.ANALYTICS_HOSTNAME || "",
   },
 
+  // postgres: {
+  //   host : process.env.POSTGRES_HOST,
+  //   user : process.env.POSTGRES_USER,
+  //   password : process.env.POSTGRES_PASSWORD,
+  //   database : process.env.POSTGRES_DATABASE || "analytics-reporter",
+  // },
+
   postgres: {
-    host : process.env.POSTGRES_HOST,
-    user : process.env.POSTGRES_USER,
-    password : process.env.POSTGRES_PASSWORD,
-    database : process.env.POSTGRES_DATABASE || "analytics-reporter",
-  },
+    host : VCAP_SERVICES_JSON.aws-rds[0].credentials[0].host, 
+    user : VCAP_SERVICES_JSON.aws-rds[0].credentials[0].username, 
+    password : VCAP_SERVICES_JSON.aws-rds[0].credentials[0].password,  
+    database : VCAP_SERVICES_JSON.aws-rds[0].credentials[0].db_name, 
+ },
 
   static: {
     path: '../analytics.usa.gov/',
