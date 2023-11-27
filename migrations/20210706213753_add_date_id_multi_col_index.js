f38937c12901f20540eaad7bb5f5d666414824ea
Ryan Wold <ryan.wold@gsa.gov>
2023-11-27 13:05:54 -0800

    update migration

--- migrations/20210706213753_add_date_id_multi_col_index.js
+++ migrations/20210706213753_add_date_id_multi_col_index.js
@@ -1,6 +1,10 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.table("analytics_data", table => {
+    })
 };
 
 exports.down = function(knex, Promise) {
+    return knex.schema.table("analytics_data", table => {
+    })
 };
