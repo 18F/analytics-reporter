dd803d717aba5975f3d7f4c8dc0b7e76621d023d
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-12-08 18:16:02 -0500

    no need to use the promise for now, revisit later

--- src/google-analytics/client.js
+++ src/google-analytics/client.js
@@ -1,84 +1,38 @@
 //const google = require("googleapis")
 const { BetaAnalyticsDataClient } = require("@google-analytics/data")
 const analyticsDataClient = new BetaAnalyticsDataClient();
-const GoogleAnalyticsQueryAuthorizer = require("./query-authorizer")
+// const GoogleAnalyticsQueryAuthorizer = require("./query-authorizer")
 const GoogleAnalyticsQueryBuilder = require("./query-builder")
 
 const fetchData = (report) => {
   const query = GoogleAnalyticsQueryBuilder.buildQuery(report)
-  return GoogleAnalyticsQueryAuthorizer.authorizeQuery(query).then(query => {
-    return _executeFetchDataRequest(query, { realtime: report.realtime })
-  })
+  return _get({ realtime: report.realtime }, query)
+
+  // return GoogleAnalyticsQueryAuthorizer.authorizeQuery(query).then(query => {
+    // return _executeFetchDataRequest(query, { realtime: report.realtime })
+  // })
 }
 
-const _executeFetchDataRequest = (query, { realtime }) => {
-  return new Promise((resolve, reject) => {
-    _get(realtime)(query, (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
-  })
-}
+// const _executeFetchDataRequest = (query, { realtime }) => {
+//   return new Promise((resolve, reject) => {
+//     _get(realtime, query, (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
 
-const _get = (realtime) => {
-  //const analytics = google.analytics("v3")
-  if (realtime) {
-    const [realtimeresponse] =  analyticsDataClient.runRealtimeReport({
-      property: `properties/393249053`,
-      dateRanges: [
-        {
-          startDate: '910daysAgo',
-          endDate: 'today',
-        },
-      ],
-      dimensions: [
-        {
-            name: 'date',
-          },
-      {
-        name: "deviceCategory"
-      }
-      ],
-    
-      metrics: [
-        {
-          name: 'sessions',
-        },
-      ],"limit":"5","sort":"date"
-   
-    });//analytics.data.realtime.get
-    return realtimeresponse;
-  } else {
-    const [response] =  analyticsDataClient.runReport({
-      property: `properties/393249053`,
-      dateRanges: [
-        {
-          startDate: '910daysAgo',
-          endDate: 'today',
-        },
-      ],
-      dimensions: [
-        {
-            name: 'date',
-          },
-      {
-        name: "deviceCategory"
-      }
-      ],
-    
-      metrics: [
-        {
-          name: 'sessions',
-        },
-      ],"limit":"5","sort":"date"
-   
-    });
-    return response;
-   
+  async function _get(realtime, query) {
+    if (realtime === true) {
+      const realTimeResponse = await analyticsDataClient.runRealtimeReport(query);
+      return realTimeResponse
+    } else {
+      const response = await analyticsDataClient.runReport(query);
+      return response
+    }
   }
-}
 
 module.exports = { fetchData }
