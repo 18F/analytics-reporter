7ad6364dedd3d6e415beec196c120b91432b2277
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-12-08 18:14:09 -0500

    just build the query in the reports object

--- src/google-analytics/query-builder.js
+++ src/google-analytics/query-builder.js
@@ -2,24 +2,8 @@ const config = require('../config')
 
 const buildQuery = (report) => {
   let query = Object.assign({}, report.query)
-  query = buildQueryArrays(query)
-  query.samplingLevel = "HIGHER_PRECISION";
-  query['max-results'] = query['max-results'] || 10000;
-  query.ids = config.account.ids;
-  return query
-}
-
-const buildQueryArrays = (query) => {
-  query = Object.assign({}, query)
-  if (query.dimensions) {
-    query.dimensions = query.dimensions.join(",")
-  }
-  if (query.metrics) {
-    query.metrics = query.metrics.join(",")
-  }
-  if (query.filters) {
-    query.filters = query.filters.join(";")
-  }
+  query.limit = query['max-results'] || 10
+  query.property = `properties/${config.account.ids}`
   return query
 }
 
