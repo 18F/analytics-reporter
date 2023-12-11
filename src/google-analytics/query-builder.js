87fb391a239ab2b9c0a82c3304943e6f24bb2f0b
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-12-11 14:28:38 -0500

    revert and include a few more items from original query build

--- src/google-analytics/query-builder.js
+++ src/google-analytics/query-builder.js
@@ -2,8 +2,10 @@ const config = require('../config')
 
 const buildQuery = (report) => {
   let query = Object.assign({}, report.query)
-  query.limit = query['max-results'] || 10
+  query.samplingLevel = "HIGHER_PRECISION";
+  query.limit = query['limit'] || "10"
   query.property = `properties/${config.account.ids}`
+  query.ids = config.account.ids;
   return query
 }
 
