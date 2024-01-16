fc8631b1271ddc14930a0e578734fccb322529c1
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-01-16 10:27:31 -0500

    remove sampling level default

--- src/google-analytics/query-builder.js
+++ src/google-analytics/query-builder.js
@@ -2,7 +2,6 @@ const config = require('../config')
 
 const buildQuery = (report) => {
   let query = Object.assign({}, report.query)
-  query.samplingLevel = "HIGHER_PRECISION";
   query.limit = query['limit'] || "10000"
   query.property = `properties/${config.account.ids}`
   query.ids = config.account.ids;
