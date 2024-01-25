bd9402ab014e511c6c7285c2e9b2c34db5d7f52e
Michael Levin <michael.levin@gsa.gov>
2024-01-25 09:04:21 -0500

    Add ability to run tests locally with docker
    Create a test docker compose file with a test DB, add instructions to the README for running local tests

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,7 @@ module.exports = {
   test: {
     client: 'postgresql',
     connection: {
+      host: '127.0.0.1',
       database: "analytics_reporter_test",
       user: process.env.CIRCLECI ? "postgres" : config.postgres.user,
       password: process.env.POSTGRES_PASSWORD,
@@ -26,7 +27,7 @@ module.exports = {
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       port: 5432,
-      ssl : true
+      ssl: true
     }
   },
 }
