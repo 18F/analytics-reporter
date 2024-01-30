3a1fcc4b396de4b63109b722e116140759eee7d5
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-01-30 15:48:46 -0500

    not using version col

--- src/publish/postgres.js
+++ src/publish/postgres.js
@@ -84,7 +84,6 @@ const _rowForDataPoint = ({ results, dataPoint }) => {
   const row = _dataForDataPoint(dataPoint)
   row.report_name = results.name
   row.report_agency = results.agency
-  row.version = '2' // this should match analytics-reporter-api version, only use non-zero semantic versions, ie 2.1 || 2.1.1
   return row
 }
 
