0381e771e63c30147f6f0440959fc1aed5585139
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-01-30 15:49:46 -0500

    migrate date_time to date

---
+++ migrations/20240130203237_rename_date_time_to_date_ga4.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.raw("ALTER TABLE analytics_data_ga4 ALTER COLUMN date TYPE date")
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+   return knex.schema.raw("ALTER TABLE analytics_data_ga4 ALTER COLUMN date TYPE timestamp with time zone")
+};
