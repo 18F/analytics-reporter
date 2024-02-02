aad77015e405b30bf35d96d02806480fd6d5766f
Michael Levin <michael.levin@gsa.gov>
2024-02-02 12:26:29 -0500

    Convert S3 publish to use AWS SDK v3 style commands

--- src/publish/s3.js
+++ src/publish/s3.js
@@ -1,10 +1,10 @@
-const AWS = require("aws-sdk")
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const zlib = require("zlib")
 const config = require("../config")
 const logger = require('../../src/logger').initialize();
 
 // This is the case where using custom s3 api-like services like minio.
-const conf = {
+const s3config = {
   accessKeyId: config.aws.accessKeyId,
   secretAccessKey: config.aws.secretAccessKey,
   endpoint: config.aws.endpoint,
@@ -12,23 +12,24 @@ const conf = {
   signatureVersion: config.aws.signatureVersion,
 };
 
-const S3 = new AWS.S3(conf);
-const publish = (report, results, { format }) => {
+const s3Client = new S3Client(s3config);
+const publish = async (report, results, { format }) => {
   logger.debug(
     "[" + report.name + "] Publishing to " + config.aws.bucket + "..."
   );
 
-  return _compress(results).then((compressed) => {
-    return S3.putObject({
-      Bucket: config.aws.bucket,
-      Key: config.aws.path + "/" + report.name + "." + format,
-      Body: compressed,
-      ContentType: _mime(format),
-      ContentEncoding: "gzip",
-      ACL: "public-read",
-      CacheControl: "max-age=" + (config.aws.cache || 0),
-    }).promise();
-  });
+  const compressed = await _compress(results);
+  const command = new PutObjectCommand({
+    Bucket: config.aws.bucket,
+    Key: config.aws.path + "/" + report.name + "." + format,
+    Body: compressed,
+    ContentType: _mime(format),
+    ContentEncoding: "gzip",
+    ACL: "public-read",
+    CacheControl: "max-age=" + (config.aws.cache || 0),
+  })
+
+  return s3Client.send(command);
 };
 
 const _compress = (data) => {
