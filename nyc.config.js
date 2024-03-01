3f3b113a4de60a768d06064f07ab3f32bf0193f1
Michael Levin <michael.levin@gsa.gov>
2024-03-01 09:18:21 -0500

    Fix lint issue with nyc config

--- nyc.config.js
+++ nyc.config.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /**
  * Configures the code coverage tool NYC.
@@ -6,20 +6,20 @@
 module.exports = {
   all: true,
   exclude: [
-    'coverage',
-    'eslint.config.js',
-    'knexfile.js',
-    'knexfile.cloudgov.js',
-    'migrations',
-    'newrelic.js',
-    'nyc.config.js',
-    'node_modules',
-    'test',
+    "coverage",
+    "eslint.config.js",
+    "knexfile.js",
+    "knexfile.cloudgov.js",
+    "migrations",
+    "newrelic.js",
+    "nyc.config.js",
+    "node_modules",
+    "test",
     // Ignore UA because it will be deprecated
-    'ua'
+    "ua",
   ],
   branches: 100,
   functions: 100,
   lines: 100,
-  statements: 100
-}
+  statements: 100,
+};
