e2b994243561ba3572333114f6d1e2fd3402be41
Michael Levin <michael.levin@gsa.gov>
2024-08-23 08:49:49 -0400

    Add logging for dates and queue connection string for local connect

--- index.js
+++ index.js
@@ -143,12 +143,12 @@ async function runQueuePublish(options = {}) {
           ),
           {
             priority: _messagePriority(reportConfig),
-            singletonKey: `${appConfig.scriptName}-${agency.agencyName}-${reportConfig.name}`,
+            singletonKey: `${appConfig.scriptName}-${agency.agencyName}-${reportConfig.name}-${reportConfig.query.dateRanges[0].startDate}`,
           },
         );
         if (jobId) {
           reportLogger.info(
-            `Created job in queue: ${queue} with job ID: ${jobId}`,
+            `Created job in queue: ${queue} with job ID: ${jobId} for ${reportConfig.query.dateRanges[0].startDate}`,
           );
         } else {
           reportLogger.info(`Found a duplicate job in queue: ${queue}`);
@@ -238,7 +238,9 @@ async function runQueueConsume() {
       queue,
       { newJobCheckIntervalSeconds: 1 },
       async (message) => {
-        appLogger.info("Queue message received");
+        appLogger.info(
+          `Queue message received for ${message.data.reportConfig.query.dateRanges[0].startDate}`,
+        );
         process.env.AGENCY_NAME = message.data.agencyName;
         process.env.ANALYTICS_REPORT_IDS = message.data.analyticsReportIds;
         process.env.AWS_BUCKET_PATH = message.data.awsBucketPath;
