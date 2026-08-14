65466bcef9cdd42a5e5fa7200495ca9d0036aef3
Shelley Nason <shelley.nason@gsa.gov>
2026-08-14 18:57:14 -0400

    Configure New Relic to use the egress proxy (#1097)

--- newrelic.js
+++ newrelic.js
@@ -1,6 +1,10 @@
 exports.config = {
   app_name: [process.env.NEW_RELIC_APP_NAME],
   license_key: process.env.NEW_RELIC_LICENSE_KEY,
+  proxy_host: process.env.PROXY_FQDN,
+  proxy_port: process.env.PROXY_PORT,
+  proxy_user: process.env.PROXY_USERNAME,
+  proxy_pass: process.env.PROXY_PASSWORD,
   logging: {
     level: "info",
     filepath: "stdout",
