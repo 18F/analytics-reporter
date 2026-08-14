2a88cdd3e0a2d0dc0396f98a1bc2c71af157797a
Shelley Nason <shelley.nason@gsa.gov>
2026-08-14 19:10:56 -0400

    Configure New Relic to use egress proxy, differently

--- newrelic.js
+++ newrelic.js
@@ -1,10 +1,7 @@
 exports.config = {
   app_name: [process.env.NEW_RELIC_APP_NAME],
   license_key: process.env.NEW_RELIC_LICENSE_KEY,
-  proxy_host: process.env.PROXY_FQDN,
-  proxy_port: process.env.PROXY_PORT,
-  proxy_user: process.env.PROXY_USERNAME,
-  proxy_pass: process.env.PROXY_PASSWORD,
+  proxy: `http://${process.env.PROXY_USERNAME}:${process.env.PROXY_PASSWORD}@${process.env.PROXY_FQDN}:${process.env.PROXY_PORT}`,
   logging: {
     level: "info",
     filepath: "stdout",
